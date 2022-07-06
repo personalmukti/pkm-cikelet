d55c4a6dce15075d960271dc85fdcf29ca4cb77a
Mukti <personal.mukti@gmail.com>
2022-07-06 21:25:01 +0700

    Adding tendis and profile page.

--- application/controllers/BackendControl.php
+++ application/controllers/BackendControl.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class BackendControl extends CI_Controller {
+class BackendControl extends CI_Controller
+{
 
 	private $filename = "import_data"; // Kita tentukan nama filenya
 
@@ -12,9 +13,9 @@ class BackendControl extends CI_Controller {
 		$this->load->model('M_berita');
 		$this->load->library('upload');
 		$this->load->model('Tendis_model');
-		
 
-		if ($this->session->userdata('status')!='login'){
+
+		if ($this->session->userdata('status') != 'login') {
 			redirect('sign-in');
 		}
 	}
@@ -36,39 +37,38 @@ class BackendControl extends CI_Controller {
 	public function publish()
 	{
 		$config['upload_path'] = './assets/images/'; //path folder
-	    $config['allowed_types'] = 'gif|jpg|png|jpeg|bmp'; //type yang dapat diakses bisa anda sesuaikan
-	    $config['encrypt_name'] = TRUE; //nama yang terupload nantinya
+		$config['allowed_types'] = 'gif|jpg|png|jpeg|bmp'; //type yang dapat diakses bisa anda sesuaikan
+		$config['encrypt_name'] = TRUE; //nama yang terupload nantinya
 
-	    $this->upload->initialize($config);
-	    if(!empty($_FILES['filefoto']['name'])){
-	        if ($this->upload->do_upload('filefoto')){
-	        	$gbr = $this->upload->data();
-	            //Compress Image
-	            $config['image_library']='gd2';
-	            $config['source_image']='./assets/images/'.$gbr['file_name'];
-	            $config['create_thumb']= FALSE;
-	            $config['maintain_ratio']= FALSE;
-	            $config['quality']= '60%';
-	            $config['width']= 710;
-	            $config['height']= 420;
-	            $config['new_image']= './assets/images/'.$gbr['file_name'];
-	            $this->load->library('image_lib', $config);
-	            $this->image_lib->resize();
+		$this->upload->initialize($config);
+		if (!empty($_FILES['filefoto']['name'])) {
+			if ($this->upload->do_upload('filefoto')) {
+				$gbr = $this->upload->data();
+				//Compress Image
+				$config['image_library'] = 'gd2';
+				$config['source_image'] = './assets/images/' . $gbr['file_name'];
+				$config['create_thumb'] = FALSE;
+				$config['maintain_ratio'] = FALSE;
+				$config['quality'] = '60%';
+				$config['width'] = 710;
+				$config['height'] = 420;
+				$config['new_image'] = './assets/images/' . $gbr['file_name'];
+				$this->load->library('image_lib', $config);
+				$this->image_lib->resize();
 
-	            $gambar=$gbr['file_name'];
-                $jdl=$this->input->post('judul');
-                $berita=$this->input->post('berita');
+				$gambar = $gbr['file_name'];
+				$jdl = $this->input->post('judul');
+				$berita = $this->input->post('berita');
 
-				$this->M_berita->simpan_berita($jdl,$berita,$gambar);
+				$this->M_berita->simpan_berita($jdl, $berita, $gambar);
 
 				redirect('post-berita');
-		}else{
+			} else {
+				redirect('post-berita');
+			}
+		} else {
 			redirect('post-berita');
-	    }
-	                 
-	    }else{
-			redirect('post-berita');
-		}	
+		}
 	}
 
 	function fetchnews()
@@ -82,14 +82,13 @@ class BackendControl extends CI_Controller {
 
 	function delete($id)
 	{
-		$query = $this->db->query('select berita_image from tbl_berita where berita_id='.$id);
+		$query = $this->db->query('select berita_image from tbl_berita where berita_id=' . $id);
 
-		foreach ($query->result() as $row)
-			{
-					$imgname = $row->berita_image;
-			}
+		foreach ($query->result() as $row) {
+			$imgname = $row->berita_image;
+		}
 
-		$target = './assets/images/'.$imgname;
+		$target = './assets/images/' . $imgname;
 		unlink($target);
 
 		$this->M_berita->remove($id);
@@ -105,6 +104,12 @@ class BackendControl extends CI_Controller {
 		$this->template->load('back/layout/template', 'back/ttendis', $data);
 	}
 
+	function profilepage()
+	{
+		$data['title'] = 'Profil || Admin PKM Cikelet';
+
+		$this->template->load('back/layout/template', 'back/profil', $data);
+	}
 }
 
 /* End of file BackendControl.php */
--- application/views/back/layout/_sidebar.php
+++ application/views/back/layout/_sidebar.php
@@ -1,72 +1,72 @@
 	<!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+	<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url(); ?>admin">
-                <div class="sidebar-brand-icon rotate-n-15">
-                </div>
-                <div class="sidebar-brand-text mx-3">Admin Dashboard</div>
-            </a>
+	    <!-- Sidebar - Brand -->
+	    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url(); ?>admin">
+	        <div class="sidebar-brand-icon rotate-n-15">
+	        </div>
+	        <div class="sidebar-brand-text mx-3">Admin Dashboard</div>
+	    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+	    <!-- Divider -->
+	    <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="<?php echo base_url(); ?>backendcontrol">
-                    <i class="fas fa-fw fa-home"></i>
-                    <span>Home</span></a>
-            </li>
+	    <!-- Nav Item - Dashboard -->
+	    <li class="nav-item active">
+	        <a class="nav-link" href="<?php echo base_url(); ?>backendcontrol">
+	            <i class="fas fa-fw fa-home"></i>
+	            <span>Home</span></a>
+	    </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+	    <!-- Divider -->
+	    <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Manajemen Konten
-            </div>
+	    <!-- Heading -->
+	    <div class="sidebar-heading">
+	        Manajemen Konten
+	    </div>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>post-berita">
-                    <i class="fas fa-fw fa-newspaper"></i>
-                    <span>Post Berita</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>data-berita">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tabel Berita</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>kelola-tendis">
-                    <i class="fas fa-fw fa-user-md"></i>
-                    <span>Kelola Tenaga Medis</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>backendcontrol">
-                    <i class="fas fa-fw fa-images"></i>
-                    <span>Kelola Penghargaan</span></a>
-            </li>
+	    <!-- Nav Item - Pages Collapse Menu -->
+	    <li class="nav-item">
+	        <a class="nav-link" href="<?php echo base_url(); ?>post-berita">
+	            <i class="fas fa-fw fa-newspaper"></i>
+	            <span>Post Berita</span></a>
+	    </li>
+	    <li class="nav-item">
+	        <a class="nav-link" href="<?php echo base_url(); ?>data-berita">
+	            <i class="fas fa-fw fa-table"></i>
+	            <span>Tabel Berita</span></a>
+	    </li>
+	    <li class="nav-item">
+	        <a class="nav-link" href="<?php echo base_url(); ?>kelola-tendis">
+	            <i class="fas fa-fw fa-user-md"></i>
+	            <span>Kelola Tenaga Medis</span></a>
+	    </li>
+	    <!--<li class="nav-item">
+	        <a class="nav-link" href="<?php echo base_url(); ?>backendcontrol">
+	            <i class="fas fa-fw fa-images"></i>
+	            <span>Kelola Penghargaan</span></a>
+	    </li>-->
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+	    <!-- Divider -->
+	    <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Manajemen Akun
-            </div>
+	    <!-- Heading -->
+	    <div class="sidebar-heading">
+	        Manajemen Akun
+	    </div>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>profile">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>Profil</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url(); ?>logout">
-                    <i class="fas fa-fw fa-arrow-circle-left"></i>
-                    <span>Logout</span></a>
-            </li>
+	    <!-- Nav Item - Pages Collapse Menu -->
+	    <li class="nav-item">
+	        <a class="nav-link" href="<?php echo base_url(); ?>profil">
+	            <i class="fas fa-fw fa-user"></i>
+	            <span>Profil</span></a>
+	    </li>
+	    <li class="nav-item">
+	        <a class="nav-link" href="<?php echo base_url(); ?>logout">
+	            <i class="fas fa-fw fa-arrow-circle-left"></i>
+	            <span>Logout</span></a>
+	    </li>
 
-        </ul>
-        <!-- End of Sidebar -->
+	</ul>
+	<!-- End of Sidebar -->
